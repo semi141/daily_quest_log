416fdc0d1067f44a8c4d96d6edf3f99061e0371d
セミ141 <whompsfortress141@gmail.com>
2026-03-04 14:17:13 +0900

    Add tag associations between Log and Tag

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,4 @@
 class Log < ApplicationRecord
+  has_many :log_tags
+  has_many :tags, through: :log_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :log_tags
+  has_many :logs, through: :log_tags
 end
