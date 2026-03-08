80fe38c6e98c94c083b903f896e34f00e1f4a8b9
セミ141 <whompsfortress141@gmail.com>
2026-03-08 17:07:51 +0900

    add duration helper and implement formatted time display

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,2 +1,14 @@
 module LogsHelper
+  def format_duration(minutes)
+    return "" if minutes.nil?
+
+    hours = minutes / 60
+    mins = minutes % 60
+
+    if hours > 0
+      "#{hours}時間#{mins}分"
+    else
+      "#{mins}分"
+    end
+  end
 end
